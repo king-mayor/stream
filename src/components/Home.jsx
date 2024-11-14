8d719ce2ce8f5da397dddf7d0c61555b06502e61
king-mayor <99553727+king-mayor@users.noreply.github.com>
2024-11-13 16:55:44 -0800

    updated animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "./Hero";
 import Latest from "./Latest";
 import NowPlaying from "./NowPlaying";
@@ -6,19 +6,35 @@ import Upcomming from "./Upcomming";
 import Popular from "./Popular";
 import Footer from "./Footer";
 import ScrollToTop from "./ScrollToTop";
+import RingLoader from "react-spinners/RingLoader";
+
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
-    <div className="bg-neutral-900 h-screen">
-      <div className="overflow-x-hidden bg-neutral-900">
-        <Hero />
-        <Upcomming />
-        <Latest />
-        <NowPlaying />
-        <Popular />
-        <Footer />
-        <ScrollToTop />
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <div className="bg-neutral-900 flex justify-center items-center w-full h-screen">
+          <RingLoader color="darkred" size={70} />
+        </div>
+      ) : (
+        <div className="bg-neutral-900 h-screen">
+          <div className="overflow-x-hidden bg-neutral-900">
+            <Hero />
+            <Upcomming />
+            <Latest />
+            <NowPlaying />
+            <Popular />
+            <Footer />
+            <ScrollToTop />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,43 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 import Footer from "./Footer";
 import { MovieData } from "./MovieData";
 import ScrollToTop from "./ScrollToTop";
 
 const Movies = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
     <>
-      <div className="bg-neutral-900 h-screen">
-        <div className="bg-neutral-900 w-full">
-          <div className="mx-auto container grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-items-center gap-6 py-28 bg-neutral-900">
-            {MovieData.map((data) => (
-              <div key={data.id} className=" text-center">
-                <Link to={`/movie/${data.id}`}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-                    alt="#movie"
-                    className="cursor-pointer lg:hover:-translate-y-4 hover:-translate-y-2"
-                  />
-                </Link>
-                <div className="py-2">
-                  <h2 className="text-md text-white font-bold">
-                    {data.original_title}
-                  </h2>
-                  <h2 className="text-md text-red-600 font-bold">
-                    {data.release_date}
-                  </h2>
+      {loading ? (
+        <div className="flex justify-center items-center bg-neutral-900 h-screen w-full">
+          <ClimbingBoxLoader color="darkred" size={36} className="text-7xl" />
+        </div>
+      ) : (
+        <div className="bg-neutral-900 h-screen">
+          <div className="bg-neutral-900 w-full">
+            <div className="mx-auto container grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-items-center gap-6 py-28 bg-neutral-900">
+              {MovieData.map((data) => (
+                <div key={data.id} className=" text-center">
+                  <Link to={`/movie/${data.id}`}>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+                      alt="#movie"
+                      className="cursor-pointer lg:hover:-translate-y-4 hover:-translate-y-2"
+                    />
+                  </Link>
+                  <div className="py-2">
+                    <h2 className="text-md text-white font-bold">
+                      {data.original_title}
+                    </h2>
+                    <h2 className="text-md text-red-600 font-bold">
+                      {data.release_date}
+                    </h2>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-          <div className="bg-neutral-900 py-10">
-            <Footer />
-          </div>
-          <div>
-            <ScrollToTop />
+              ))}
+            </div>
+            <div className="bg-neutral-900 py-10">
+              <Footer />
+            </div>
+            <div>
+              <ScrollToTop />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
